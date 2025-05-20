685066aa6232528a1171d1e6e9133acde9c327cc
tsgamage <thevidu2007627@gmail.com>
2025-05-20 15:27:00 +0530

    Make player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./assets/components/Player/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <h1>React Tic-Tac-Toe</h1>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="O" />
+          <Player name="Player 2" symbol="X" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
