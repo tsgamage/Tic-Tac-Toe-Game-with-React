25d6b567b05488b89b703000fdd8bb46791b7d21
tsgamage <thevidu2007627@gmail.com>
2025-05-22 21:25:28 +0530

    fix: Player can click a button multiple times
    Add some condition when rendering button. now if a button already clicked, it will disabled. Add a key to log component and fix key missing error

--- src/components/Log/Log.jsx
+++ src/components/Log/Log.jsx
@@ -2,7 +2,10 @@ export default function Log({ turns }) {
   return (
     <ol id="log">
       {turns.map((turn, turnIndex) => (
-        <li className={turnIndex === 0 ? "highlighted" : undefined}>
+        <li
+          key={`${turn.square.row}${turn.square.col}`}
+          className={turnIndex === 0 ? "highlighted" : undefined}
+        >
           {turn.player} clicked on square ({turn.square.col + 1},
           {turn.square.row + 1})
         </li>
