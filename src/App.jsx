01e6f0c41c579724a40ef1a3b7520648a573b402
tsgamage <thevidu2007627@gmail.com>
2025-05-23 23:26:48 +0530

    feat: Add a logic to win the game and GameBoard changes
    Add a winning-combinations to the game and write a login in App component to check if player won, in every button click.  Lift up the computed values in GameBoard component to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player/Player";
 import GameBoard from "./components/GameBoard/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -15,8 +22,15 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((preGameturn) => {
@@ -28,6 +42,23 @@ function App() {
       ];
       return updatedGameTurns;
     });
+
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSqureSymbol =
+        gameBoard[combination[0].row][combination[0].column];
+      const secondSqureSymbol =
+        gameBoard[combination[1].row][combination[1].column];
+      const thirdSqureSymbol =
+        gameBoard[combination[2].row][combination[2].column];
+
+      if (
+        firstSqureSymbol &&
+        firstSqureSymbol === secondSqureSymbol &&
+        firstSqureSymbol === thirdSqureSymbol
+      ) {
+        console.log(firstSqureSymbol + "Won");
+      }
+    }
   }
 
   return (
@@ -45,7 +76,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
